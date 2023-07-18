93181eda46e032f4f5b980e98a2c23421fe06caa
Vincent Billerey <vincent.billerey.web@gmail.com>
2023-07-18 17:53:38 +0200

    Responsive tablette fait. Début de la version téléphone

--- wp-content/themes/foce-child/template-parts/images-burger.php
+++ wp-content/themes/foce-child/template-parts/images-burger.php
@@ -1,9 +1,9 @@
-<img class ="logo-burger position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/logo-burger.png'; ?>">
-<img class ="orchid-flower-burger rotate position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/orchid.png'; ?>">
-<img class ="petal-flower-burger rotate position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/flower.png'; ?>">
-<img class ="sun-flower-burger rotate position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/sunflower.png'; ?>">
-<img class ="pink-flower-burger rotate position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/pink-flower.png'; ?>">
-<img class ="hibiscus-flower-burger rotate position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/hibiscus-footer.png'; ?>">
-<img class ="blue-cat-burger float position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/blue-cat.png'; ?>">
-<img class ="orange-cat-burger float position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/orange-cat.png'; ?>">
-<img class ="black-cat-burger float position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/black-cat.png'; ?>">
+<img class ="burger-open__logo position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/logo-burger.png'; ?>">
+<img class ="burger-open__orchid-flower rotate position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/orchid.png'; ?>">
+<img class ="burger-open__petal-flower rotate position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/flower.png'; ?>">
+<img class ="burger-open__sun-flower rotate position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/sunflower.png'; ?>">
+<img class ="burger-open__pink-flower rotate position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/pink-flower.png'; ?>">
+<img class ="burger-open__hibiscus-flower rotate position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/hibiscus-footer.png'; ?>">
+<img class ="burger-open__blue-cat float position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/blue-cat.png'; ?>">
+<img class ="burger-open__orange-cat float position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/orange-cat.png'; ?>">
+<img class ="burger-open__black-cat float position" src="<?php echo get_theme_file_uri() . '/assets/images/menu-burger/black-cat.png'; ?>">
